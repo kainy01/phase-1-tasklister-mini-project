84c6fbd46cdedb8dbc3f3f9246813338301a2902
kainy01 <kainykawai@gmail.com>
2023-04-20 17:43:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildToDo(e.target.description.value);
+  });
 });
+
+function buildToDo (todo){
+  let li=document.createElement('li')
+  let btn=document.createElement('button')
+  btn.addEventListener('click',handeDelete)
+  btn.textContent='x'
+  li.textContent=`${todo} `
+  li.appendChild(btn)
+  document.getElementById('tasks').appendChild(li)
+
+}
+
+function handeDelete(e){
+  e.target.parentNode.remove()
+}
